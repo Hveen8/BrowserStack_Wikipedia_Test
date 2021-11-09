a24ee57c9b7b97ef83dde813d0830e4d809b8890
Hveen8 <harry.vanderveen8@gmail.com>
2021-11-09 10:56:17 -0500

    Adding IOS test

---
+++ app/src/main/java/com/example/browserstackwikipediatest/IOSTest.java
@@ -0,0 +1,52 @@
+package com.example.browserstackwikipediatest;
+
+import java.net.URL;
+import java.util.List;
+import java.net.MalformedURLException;
+
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import io.appium.java_client.MobileBy;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.ios.IOSElement;
+
+public class IOSTest {
+
+    public static String userName = "harryvanderveen_5FiwGa";
+    public static String accessKey = "e4EbPv9rtnWVeTNhWV5T";
+
+    public static void main(String args[]) throws MalformedURLException, InterruptedException {
+        DesiredCapabilities caps = new DesiredCapabilities();
+
+        caps.setCapability("device", "iPhone 7 Plus");
+        caps.setCapability("os_version", "10");
+        caps.setCapability("project", "My First Project");
+        caps.setCapability("build", "Ran from IDE");
+        caps.setCapability("name", "Sample IOS Test");
+        caps.setCapability("app", "bs://2245373184b5f86fbb5cb9c662015acd17af291a");
+
+        IOSDriver<IOSElement> driver = new IOSDriver<IOSElement>(new URL("https://"+userName+":"+accessKey+"@hub-cloud.browserstack.com/wd/hub"), caps);
+
+        IOSElement textButton = (IOSElement) new WebDriverWait(driver, 30).until(
+                ExpectedConditions.elementToBeClickable(MobileBy.AccessibilityId("Text Button")));
+        textButton.click();
+        IOSElement textInput = (IOSElement) new WebDriverWait(driver, 30).until(
+                ExpectedConditions.elementToBeClickable(MobileBy.AccessibilityId("Text Input")));
+        textInput.sendKeys("hello@browserstack.com\n");
+
+        Thread.sleep(5000);
+
+        IOSElement textOutput = (IOSElement) new WebDriverWait(driver, 30).until(
+                ExpectedConditions.elementToBeClickable(MobileBy.AccessibilityId("Text Output")));
+
+        if(textOutput != null && textOutput.getText().equals("hello@browserstack.com"))
+            assert(true);
+        else
+            assert(false);
+
+        // The driver.quit statement is required, otherwise the test continues to execute, leading to a timeout.
+        driver.quit();
+    }
+}
